c206655a56a13115bd9fe1f46cafff5c7e322745
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2025-01-26 18:21:38 +0530

    use local system time

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, func
+from sqlalchemy import Column, Integer, String, Text, DateTime
 from database import Base
+from datetime import datetime
 
 class Message(Base):
     __tablename__ = "messages"
     id = Column(Integer, primary_key=True, index=True)
     user_message = Column(Text, nullable=False)
     bot_response = Column(Text, nullable=False)
-    timestamp = Column(DateTime(timezone=True), server_default=func.now())
+    timestamp = Column(DateTime(timezone=True), default=datetime.now)
 
 class Data(Base):
     __tablename__ = "data"
